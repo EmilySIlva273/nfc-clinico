b15d63c53f08eeae88b04a7fa4d96ba07c19e265
Emily Silva <emily100prerbd@gmail.com>
2026-05-03 16:59:24 -0500

    solucion supa

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://sneybhegyfavtbvosvjt.supabase.co/rest/v1/"
+const supabaseUrl = "https://sneybhegyfavtbvosvjt.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuZXliaGVneWZhdnRidm9zdmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTQzOTMsImV4cCI6MjA5MzM5MDM5M30.J-DBmZoOJx6Q-S-HDZHVDJZXDxaVhklOdqgpA0CBY1g"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
